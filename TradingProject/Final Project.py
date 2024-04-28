7908bc0dd5ef84737f9d6e5afc04344d30087cf7
kairjiang <jiangkr05@gmail.com>
2024-04-28 16:19:50 -0400

    Clean up code

--- TradingProject/Final Project.py
+++ TradingProject/Final Project.py
@@ -18,9 +18,6 @@ end_date = dt.datetime(2023, 12, 31)
 data = yf.download(stock_symbols, start=start_date, end=end_date)['Adj Close']
 risk_free = yf.download(risk_free_symbol, start=start_date, end=end_date)['Adj Close']
 
-# print(data)
-# print(risk_free)
-
 # Set display options to show all rows and columns
 pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
@@ -30,31 +27,21 @@ daily_returns = data.pct_change().ffill()
 risk_free_daily_returns = risk_free.shift(1) / 36500
 
 daily_cum = (1 + daily_returns).cumprod()
-
 constant_allocation = 0
 
 for symbol in stock_symbols:
     constant_allocation += daily_cum[symbol] * 0.2
 
-print(constant_allocation.iloc[-1])
-
+# print(constant_allocation.iloc[-1])
 
 # Align dimensions and compute excess returns
 excess_returns = daily_returns.subtract(risk_free_daily_returns, axis=0)
 
-# print(excess_returns)
-
 # Calculate 200-day moving averages
 MA200 = data.rolling(window=200).mean()
 
-# print(MA200)
-
 # Determine positions: 1 if stock price > moving average, 0 otherwise
 positions = (data.shift(1) > MA200).astype(int)
-
-# with np.printoptions(threshold=np.inf):
-#     print(positions)
-
 
 # Allocate portfolio: 20% to stock or 0% if below moving average
 portfolio_allocations = positions.multiply(0.2)
@@ -62,28 +49,20 @@ portfolio_allocations = positions.multiply(0.2)
 # Calculate portfolio returns: element-wise multiply and row-wise sum
 portfolio_returns = (portfolio_allocations.shift(1) * daily_returns).sum(axis=1)
 
-# print(portfolio_returns)
-
 # Statistics
-mean_return = portfolio_returns.mean()
-std_deviation = portfolio_returns.std()
-
-# constant_allocation = daily_returns.mean(axis=1) * 0.2
+portfolio_mean = portfolio_returns.mean()
+portfolio_std = portfolio_returns.std()
 constant_mean = constant_allocation.mean()
 constant_std = constant_allocation.std()
-
-risk_free_allocation = risk_free_daily_returns.mean(axis=0) * 0.8
-
 
 # Calculate cumulative returns
 cumulative_constant = constant_allocation
 cumulative_strategy = (1 + portfolio_returns).cumprod()
 
-
 # Plotting
 plt.figure(figsize=(10, 6))
 plt.plot(cumulative_constant, label='Constant Allocation Strategy')
-# plt.plot(cumulative_strategy, label='Moving Average Strategy')
+plt.plot(cumulative_strategy, label='Moving Average Strategy')
 plt.legend()
 plt.title('Cumulative Performance Comparison')
 plt.xlabel('Date')
